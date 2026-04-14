505041611162c27a732c880a17c2bfd114491f69
DiegoTCobba <diegot.cobba@gmail.com>
2026-04-14 09:53:32 -0500

    Update credit.py

--- backend/routes/credit.py
+++ backend/routes/credit.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
+from services.kashio_service import ejecutar_credito
 
+router = APIRouter()
+
+@router.post("/")
+def credit(data: dict):
+    return ejecutar_credito(data)
