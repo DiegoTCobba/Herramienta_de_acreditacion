7cff1cbce90133690b63ec0c83a69f9a207d81b9
DiegoTCobba <diegot.cobba@gmail.com>
2026-04-14 09:53:14 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
+from routes import credit, debit, tin
 
+app = FastAPI(title="Kashio Internal System")
+
+app.include_router(credit.router, prefix="/credit")
+app.include_router(debit.router, prefix="/debit")
+app.include_router(tin.router, prefix="/tin")
