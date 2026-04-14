5d2e17366727e538e5399e2b005d26facb3c0128
DiegoTCobba <diegot.cobba@gmail.com>
2026-04-14 09:55:36 -0500

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE operations (
+    id SERIAL PRIMARY KEY,
+    tipo TEXT,
+    cliente TEXT,
+    monto NUMERIC,
+    moneda TEXT,
+    estado TEXT,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    respuesta JSONB
+);
