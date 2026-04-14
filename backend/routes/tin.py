6e0074b33106fe2a841c6e320eb7688ddf73f39d
DiegoTCobba <diegot.cobba@gmail.com>
2026-04-14 09:54:07 -0500

    Update tin.py

--- backend/routes/tin.py
+++ backend/routes/tin.py
@@ -1,1 +1,10 @@
+from fastapi import APIRouter
+from services.parser_service import extraer_tins
 
+router = APIRouter()
+
+@router.post("/")
+def tin(data: dict):
+    texto = data.get("texto", "")
+    tins = extraer_tins(texto)
+    return {"tins": tins}
