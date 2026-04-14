4105e6c694af23bf3061fbfb8369dab03aa915c0
DiegoTCobba <diegot.cobba@gmail.com>
2026-04-14 09:53:56 -0500

    Update debit.py

--- backend/routes/debit.py
+++ backend/routes/debit.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
+from services.kashio_service import ejecutar_debito
 
+router = APIRouter()
+
+@router.post("/")
+def debit(data: dict):
+    return ejecutar_debito(data)
